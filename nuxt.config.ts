92c13e40dd2c90a89d3b0baca949d0bd5998ce1e
justpeterpan <peter.busch@posteo.net>
2024-06-08 12:49:16 +0200

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ['@nuxthub/core', "@nuxt/eslint"],
+  hub: {
+    database: true,
+    kv: true,
+    blob: true,
+    cache: true,
+  },
+  nitro: {
+    experimental: {
+      // Enable Server API documentation within NuxtHub
+      openAPI: true
+    }
+  }
+})
