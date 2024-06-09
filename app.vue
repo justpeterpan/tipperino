cbf63b9d69e320b5939aa20044ae06e0990aceab
justpeterpan <peter.busch@posteo.net>
2024-06-10 01:10:06 +0200

    chore: remove soon message

--- app.vue
+++ app.vue
@@ -13,5 +13,22 @@ const menuItems = [
 </script>
 
 <template>
-  <div class="grid justify-items-center items-center h-dvh">✦ soon ✦</div>
+  <div>
+    <nav
+      class="absolute w-full px-10 h-16 mb-16 flex items-center border-b border-emerald-400/20"
+    >
+      <ul class="flex justify-between w-full">
+        <li>
+          <NuxtLink to="/">
+            <UIcon name="i-heroicons-trophy-20-solid w-6 h-6" />
+          </NuxtLink>
+        </li>
+        <li v-if="loggedIn">
+          <UserMenu :menu-items="menuItems" />
+        </li>
+      </ul>
+    </nav>
+    <NuxtPage class="pt-20 h-dvh px-4" />
+    <UNotifications />
+  </div>
 </template>
