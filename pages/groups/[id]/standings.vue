1482a34ec1d2c45ae1ad4a7405d1b3689d5b5fcc
justpeterpan <peter.busch@posteo.net>
2024-06-15 00:59:53 +0200

    feat: provide score table

--- pages/groups/[id]/standings.vue
+++ pages/groups/[id]/standings.vue
@@ -13,16 +13,43 @@ const { data } = await useFetch("/api/predictions/standings", {
 
 <template>
   <div>
-    <div class="italic font-serif text-4xl">
-      ✦ soon ✦
-      <!-- <ul class="w-32">
-        <li v-for="d of data" :key="d" class="flex justify-between">
-          <div>
-            {{ d }}
+    <div class="font-black text-4xl">
+      <ul>
+        <li
+          v-for="score of data?.scores"
+          :key="score.userName"
+          class="list-grid"
+        >
+          <div class="text-left font-serif italic font-thin">
+            {{ score.rank }}.
           </div>
-          <div>0</div>
+          <div class="text-left lowercase">
+            {{ score.userName }}
+            <div>
+              <div v-for="p of data?.predictions">
+                <div
+                  v-if="p.user === score.userId"
+                  class="font-thin font-serif italic text-lg mt-1"
+                >
+                  {{ p.team1Name }} {{ p.team1Score }} : {{ p.team2Score }}
+                  {{ p.team2Name }}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="text-right font-medium">{{ score.points }}</div>
         </li>
-      </ul> -->
+      </ul>
     </div>
   </div>
 </template>
+
+<style>
+.list-grid {
+  display: grid;
+  margin-bottom: 8px;
+  grid-template-columns: 50px auto 50px;
+  max-width: 380px;
+  line-height: 0.9em;
+}
+</style>
--- server/api/predictions/standings.get.ts
+++ server/api/predictions/standings.get.ts
@@ -1,14 +1,64 @@
 export default eventHandler(async (event) => {
   const { user } = await requireUserSession(event);
   const { group } = getQuery(event);
-  const users = await useDB()
-    .select({ user: tables.users.id, name: tables.users.name })
-    .from(tables.members)
-    .innerJoin(tables.users, eq(tables.members.user, tables.users.id))
-    .where(and(eq(tables.members.group, Number(group))))
-    .all();
+  const scores = await useDB()
+    .select({
+      userId: tables.users.id,
+      userName: tables.users.name,
+      points: tables.scores.points,
+      group: tables.scores.group,
+      predictions: tables.predictions,
+    })
+    .from(tables.scores)
+    .innerJoin(tables.users, eq(tables.users.id, tables.scores.user))
+    .innerJoin(
+      tables.predictions,
+      eq(tables.predictions.group, tables.scores.group)
+    )
+    .where(
+      and(
+        eq(tables.scores.group, Number(group)),
+        eq(tables.predictions.finished, 1)
+      )
+    );
 
-  return users.some((u) => u.user === user.id)
-    ? users.map((user) => user.name)
-    : [];
+  const userScore = scores.filter((s) => s.userId === user.id);
+  const [uniqueUserScore] = Array.from(
+    new Map(userScore.map((score) => [score.userId, score.points])).values()
+  );
+  const uniqueScores = Array.from(
+    new Map(scores.map((score) => [score.userId, score])).values()
+  );
+  const processedScores = processScores(uniqueScores);
+  const processedPredictions = scores.map((s) => ({
+    ...s.predictions,
+  }));
+
+  const uniquePredictions = Array.from(
+    new Map(processedPredictions.map((p) => [p.id, { ...p }])).values()
+  );
+
+  return {
+    user: uniqueUserScore,
+    scores: processedScores,
+    predictions: uniquePredictions,
+  };
 });
+
+function processScores(
+  scores: {
+    userId: string;
+    userName: string;
+    points: number;
+    group: number;
+    rank?: number;
+  }[]
+) {
+  const p = scores.toSorted((a, b) => b.points - a.points);
+
+  p.forEach((score, index) => {
+    score.rank = index + 1;
+  });
+
+  return p;
+}
